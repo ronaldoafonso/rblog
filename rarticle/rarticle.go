60c23cdcb9d9ba7e2fb51cb506e284ef6551de87
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2020-02-11 12:47:09 -0300

    Create an "article" data type.

--- rarticle/rarticle.go
+++ rarticle/rarticle.go
@@ -17,46 +17,37 @@ var (
 		"rarticle/html/index.html",
 		"rarticle/html/footer.html",
 	}
-	articles map[string]func(http.ResponseWriter, *http.Request)
+	articles map[string]article = make(map[string]article)
 )
 
+type article struct {
+    name string
+}
+
+func (a article) Handler(w http.ResponseWriter, r *http.Request) {
+	templates[3] = "rarticle/html/"+a.name+".html"
+	tmplt, err := template.ParseFiles(templates...)
+	if err == nil {
+		tmplt.ExecuteTemplate(w, "layout", "")
+	} else {
+		fmt.Println(err)
+	}
+}
+
 func init() {
-	articles = make(map[string]func(http.ResponseWriter, *http.Request))
-	articles["index"] = index
-	articles["about"] = about
+	articles["index"] = article{"index"}
+	articles["about"] = article{"about"}
 }
 
 // HandleArticle ... Handle an rblog article.
 func HandleArticle(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	if len(vars) == 0 {
-		articles["index"](w, r)
-	} else if article, ok := articles[vars["article"]]; ok {
-		article(w, r)
+		articles["index"].Handler(w, r)
+	} else if a, ok := articles[vars["article"]]; ok {
+		a.Handler(w, r)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "Page Not Found.")
-	}
-}
-
-// index ... Handle index page.
-func index(w http.ResponseWriter, r *http.Request) {
-	templates[3] = "rarticle/html/index.html"
-	indexTemplate, err := template.ParseFiles(templates...)
-	if err == nil {
-		indexTemplate.ExecuteTemplate(w, "layout", "")
-	} else {
-		fmt.Println(err)
-	}
-}
-
-// about .. Handle "about" page.
-func about(w http.ResponseWriter, r *http.Request) {
-	templates[3] = "rarticle/html/about.html"
-	aboutTemplate, err := template.ParseFiles(templates...)
-	if err == nil {
-		aboutTemplate.ExecuteTemplate(w, "layout", "")
-	} else {
-		fmt.Println(err)
 	}
 }
