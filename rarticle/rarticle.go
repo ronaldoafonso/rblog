5586bdec4cfcf4a8b22eabbc14898f5daf9be49f
Ronaldo Afonso <ronaldo@ronaldoafonso.com.br>
2020-11-01 14:10:20 -0300

    Make templates more generic by passing an article type.

--- rarticle/rarticle.go
+++ rarticle/rarticle.go
@@ -10,25 +10,26 @@ import (
 )
 
 type article struct {
-	lang string
-	name string
+	Lang string
+	Name string
 	tpls []string
+	Host string
 }
 
 func (a *article) InitTemplates() {
 	a.tpls = []string{
-		"rarticle/html/" + a.lang + "/layout.html",
-		"rarticle/html/" + a.lang + "/head.html",
-		"rarticle/html/" + a.lang + "/header.html",
-		"rarticle/html/" + a.lang + "/" + a.name + ".html",
-		"rarticle/html/" + a.lang + "/footer.html",
+		"rarticle/html/" + a.Lang + "/layout.html",
+		"rarticle/html/" + a.Lang + "/head.html",
+		"rarticle/html/" + a.Lang + "/header.html",
+		"rarticle/html/" + a.Lang + "/" + a.Name + ".html",
+		"rarticle/html/" + a.Lang + "/footer.html",
 	}
 }
 
 func (a *article) ExecuteTemplate(w http.ResponseWriter) {
 	tpl, err := template.ParseFiles(a.tpls...)
 	if err == nil {
-		tpl.ExecuteTemplate(w, "layout", "localhost")
+		tpl.ExecuteTemplate(w, "layout", a)
 	} else {
 		fmt.Println(err)
 	}
@@ -41,19 +42,18 @@ func (a article) Handler(w http.ResponseWriter, r *http.Request) {
 
 // HandleArticle ... Handle an rblog article.
 func HandleArticle(w http.ResponseWriter, r *http.Request) {
-	a := article{}
+	a := article{Host: "localhost"}
 	vars := mux.Vars(r)
 	if len(vars) == 2 {
-		a.lang = vars["lang"]
-		a.name = vars["name"]
+		a.Lang = vars["lang"]
+		a.Name = vars["name"]
 	} else if len(vars) == 0 {
-		a.lang = "en"
-		a.name = "index"
+		a.Lang = "en"
+		a.Name = "index"
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "Page Not Found.")
 		return
 	}
-
 	a.Handler(w, r)
 }
